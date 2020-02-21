025638a363238df5f9a625555c6a6f6cd4216773
Martin Kuchar <kucharmartin0@gmail.com>
2020-02-21 10:16:20 -0800

    Add random method

--- src/guessing_game/Game.java
+++ src/guessing_game/Game.java
@@ -1,9 +1,34 @@
 package guessing_game;
 
+import java.util.List;
+import java.util.Random;
+
 public class Game {
     private String guessedCity;
 
+    /**
+     * Constructor
+     * @param city
+     */
+
     public Game(String city) {
         this.guessedCity = city;
+    }
+
+    /**
+     * Method
+     * @param cities
+     * @return
+     */
+    private String randomCity(List<String> cities) {
+        Random random = new Random();
+        return cities.get(random.nextInt(List.lenght));
+    }
+    private String underscoreCity() {
+        String a = new String[randomCity().length];
+        for (int i = 0; i < this.randomCity().length; i++){
+            a += "_";
+        }
+        return a;
     }
 }
